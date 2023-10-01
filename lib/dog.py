e6ef9ca4cb6357c87fafd40c21fd969d6e1c6a90
+ <+>
2023-10-01 19:29:13 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,31 @@
 from models import Dog
 
 def create_table(base):
-    pass
+    # Create the table for the Dog model if it doesn't exist
+    base.metadata.create_all()
 
 def save(session, dog):
-    pass
+    # Add a Dog object to the session and commit it to the database
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    # Retrieve all Dog objects from the database
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    # Find a Dog by name
+    return session.query(Dog).filter_by(name=name).first()
 
 def find_by_id(session, id):
-    pass
+    # Find a Dog by its ID
+    return session.query(Dog).filter_by(id=id).first()
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    # Find a Dog by name and breed
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    # Update the breed of a Dog object
+    dog.breed = breed
+    session.commit()
--- lib/models.py
+++ lib/models.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
+# Create a SQLite database engine
+DATABASE_URL = 'sqlite:///dogs.db'  # Change the database URL as needed
+engine = create_engine(DATABASE_URL)
 
 Base = declarative_base()
 
@@ -11,3 +16,67 @@ class Dog(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+
+# Create the 'dogs' table in the database (if it doesn't exist)
+Base.metadata.create_all(engine)
+
+# Create a session to interact with the database
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# CRUD operations
+
+# Create a new dog record
+def create_dog(name, breed):
+    dog = Dog(name=name, breed=breed)
+    session.add(dog)
+    session.commit()
+
+# Retrieve all dogs from the database
+def get_all_dogs():
+    return session.query(Dog).all()
+
+# Find a dog by its ID
+def find_dog_by_id(dog_id):
+    return session.query(Dog).filter_by(id=dog_id).first()
+
+# Find a dog by its name
+def find_dog_by_name(name):
+    return session.query(Dog).filter_by(name=name).first()
+
+# Update a dog's breed by its ID
+def update_dog_breed(dog_id, new_breed):
+    dog = find_dog_by_id(dog_id)
+    if dog:
+        dog.breed = new_breed
+        session.commit()
+
+# Delete a dog by its ID
+def delete_dog_by_id(dog_id):
+    dog = find_dog_by_id(dog_id)
+    if dog:
+        session.delete(dog)
+        session.commit()
+
+# Example usage
+
+if __name__ == '__main__':
+    # Create and save new dog records
+    create_dog("Joey", "Cocker Spaniel")
+    create_dog("Fanny", "Cockapoo")
+
+    # Retrieve and print all dogs
+    all_dogs = get_all_dogs()
+    for dog in all_dogs:
+        print(f"Dog ID: {dog.id}, Name: {dog.name}, Breed: {dog.breed}")
+
+    # Update a dog's breed
+    update_dog_breed(1, "Golden Retriever")
+
+    # Delete a dog by its ID
+    delete_dog_by_id(2)
+
+    # Find and print a dog by its name
+    found_dog = find_dog_by_name("Joey")
+    if found_dog:
+        print(f"Found dog: Name: {found_dog.name}, Breed: {found_dog.breed}")
